507d1270833221ddaaca62595f221215c95f3f3f
bryant <bryant@defrag.in>
2014-07-14 02:02:25 -0400

    temporary simplifications
    - replace generalized type ctor with function type and a few other base types

--- src/algorithm-w.hs
+++ src/algorithm-w.hs
@@ -7,76 +7,78 @@ type TVarID = Int
 data Expr = Var Ident | App Expr Expr | Abs Ident Expr | Let Ident Expr Expr
 data Type =
       TypeVar TVarID
-    | TypeCtor Ident Int  -- number of type params
-    | TypeApp Type [Type]
+    | FuncType Type Type
+    | IntType
+    | BoolType
+    | ListType Type
     deriving (Show, Eq)
 data PolyType = ForAll [TVarID] Type deriving Show
 
-data Subst = TVarID :==> Type deriving Show
+type Substs = [(TVarID, Type)]
+type Context = [(Ident, PolyType)]
 data InfCtx = InfCtx { assumps :: [(Ident, PolyType)], id_gen :: TVarID }
     deriving Show
-type Infer = State.State InfCtx
+type IDGen = State.State TVarID
+
+each = flip map
+
+-- (s1 `compose` s0) `s` tvar = s1 `s` (s0 `s` tvar)
+compose :: Substs -> Substs -> Substs
+compose s1 s0 = merge s0 s1'
+    where
+    s1' = each s1 $ \(x, y) -> (x, subst s0 y)
+    merge = List.unionBy $ \x y -> fst x == fst y
 
 class HasFree t where
     free_var :: t -> [TVarID]
-    substitute :: Subst -> t -> t
+    subst :: Substs -> t -> t
 
 instance HasFree Type where
     free_var (TypeVar var) = [var]
-    free_var (TypeApp ctor tyargs) =
-        free_var ctor `List.union` concatMap free_var tyargs
+    free_var (FuncType t0 t1) = free_var t0 `List.union` free_var t1
+    free_var (ListType t) = free_var t
     free_var _ = []
 
-    substitute (tv :==> ty) t@(TypeVar tv')
-        | tv == tv' = ty
-        | otherwise = t
-    substitute s (TypeApp ctor tyargs) = TypeApp ctor' tyargs'
-        where
-        ctor' = substitute s ctor
-        tyargs' = flip map tyargs $ substitute s
-    substitute _ t = t
+    subst subs (TypeVar vid) = case List.lookup vid subs of
+        Nothing -> TypeVar vid
+        Just t -> t
+    subst subs (FuncType t0 t1) = FuncType (subst subs t0) (subst subs t1)
+    subst subs (ListType t) = ListType $ subst subs t
+    subst _ t = t
 
 instance HasFree PolyType where
-    free_var (ForAll tvars monotype) = free_var monotype List.\\ tvars
-    substitute s@(tv :==> ty) (ForAll tvars mono) = ForAll tvars' mono'
+    free_var (ForAll qvars monotype) = free_var monotype List.\\ qvars
+    subst subs (ForAll qvars mono) = ForAll qvars' mono'
         where
-        tvars' = List.delete tv tvars
-        mono' = substitute s mono
+        qvars' = qvars List.\\ map fst subs
+        mono' = subst subs mono
 
--- hindley milner requires (->)
-func_type = TypeCtor "(->)" 2
-uint = TypeCtor "uint" 0
-empty_ctx = InfCtx { assumps = [], id_gen = 0 }
-
-new_var :: Infer TVarID
+new_var :: IDGen TVarID
 new_var = do
-    vid <- id_gen `fmap` State.get
-    State.modify inc_gen
+    vid <- State.get
+    State.modify (+1)
     return vid
+
+unify :: Type -> Type -> Substs
+unify (TypeVar vid) u = [(vid, u)]
+unify u (TypeVar vid) = [(vid, u)]
+unify (FuncType t0 t1) (FuncType t0' t1') = s1 `compose` s0
+    where (s0, s1) = (unify t0 t0', unify (subst s0 t1) (subst s0 t1'))
+unify (ListType t) (ListType t') = unify t t'
+unify t t'
+    | t == t' = []
+    | otherwise = error $ "cannot unify " ++ show t ++ " with " ++ show t'
+
+infer :: Context -> Expr -> IDGen PolyType
+infer ctx (Var binding) = case List.lookup binding ctx of
+    Just ty -> inst ty
+    Nothing -> error $ "unknown variable: " ++ show binding
     where
-    inc_gen ctx = ctx { id_gen = id_gen ctx + 1 }
-
-instantiate :: PolyType -> Infer PolyType
-instantiate (ForAll qvars mono) = do
-    newqvars <- mapM (const new_var) qvars
-    let subs = zipWith (:==>) qvars $ map TypeVar newqvars
-    let newmono = List.foldl' (flip substitute) mono subs
-    return $ ForAll newqvars newmono
-
-unify :: Type -> Type -> [Subst]
-unify (TypeVar vid) u = [vid :==> u]
-unify u (TypeVar vid) = [vid :==> u]
-unify (TypeApp u tyargs) (TypeApp u' tyargs')
-    | u == u' = concat $ zipWith unify tyargs tyargs'
-unify t t' = error $ "cannot unify " ++ show t ++ " with " ++ show t'
-
-infer :: Expr -> Infer Type
-infer (Var varname) = do
-    a <- assumps `fmap` State.get
-    case List.lookup varname a of
-        Nothing -> error "unknown variable"
-        Just ty -> return ty
-infer (App expr0 expr1) = do
-    t0 <- infer expr0
-    t1 <- infer expr1
-    let subs = unify (TypeApp func_type [t0
+    inst :: PolyType -> IDGen PolyType
+    inst (ForAll qvars mono) = do
+        newvars <- mapM (const new_var) qvars
+        let subs = zipWith (\q v -> (q, TypeVar v)) qvars newvars
+        return . ForAll newvars $ subst subs mono
+infer ctx (App e0 e1) = undefined
+infer ctx (Abs bind e1) = undefined
+infer ctx (Let bind e0 e1) = undefined
